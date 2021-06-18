087ad2b956217ce9ff991465d2561bfaf3dffc2e
Alexander Meléndez <xandermelendez.2001@gmail.com>
2021-06-17 21:09:43 -0500

    Adding files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes),FormsModule,ReactiveFormsModule],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
