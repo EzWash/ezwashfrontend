2868f7cb0017fe48dc9e9f3292368e04cd8c8cde
Alexander Meléndez <xandermelendez.2001@gmail.com>
2021-06-17 21:34:20 -0500

    Fix: Fixing some bugs

---
+++ src/app/model/accounts/carwash.ts
@@ -0,0 +1,11 @@
+export interface Customer{
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  phone_number: string
+  gender: string
+  location: number
+  username: string
+  password:string
+}
